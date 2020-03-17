e54da9aff0ba428cc44c8a8a65f63febf8fdd325
Jarosław Węgliński <j.weglinski2@gmail.com>
2020-03-17 21:16:46 +0100

    init

---
+++ src/main/java/com/weglinskij/assecohometask/AssecoHomeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.weglinskij.assecohometask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssecoHomeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssecoHomeTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weglinskij/assecohometask/AssecoHomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weglinskij.assecohometask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssecoHomeTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
