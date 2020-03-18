0921ac5c20d3e191002bd97f379ab6774ffbcf07
Jarosław Węgliński <jaroslaw.weglinski@novaris.pl>
2020-03-18 03:31:56 +0100

    added random notification popping

--- src/main/java/com/weglinskij/assecohometask/view/IndexView.java
+++ src/main/java/com/weglinskij/assecohometask/view/IndexView.java
@@ -1,13 +1,22 @@
 package com.weglinskij.assecohometask.view;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.StreamRegistration;
+import com.vaadin.flow.server.StreamResource;
 import com.weglinskij.assecohometask.model.Invoice;
 import com.weglinskij.assecohometask.repository.InvoiceRepository;
 import com.weglinskij.assecohometask.view.component.EmbeddedPdfDocument;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 @Route("index")
 @RouteAlias("main")
@@ -16,26 +25,59 @@ public class IndexView extends VerticalLayout {
 
     private final InvoiceRepository invoiceRepository;
     final Grid<Invoice> grid;
+    final EmbeddedPdfDocument[] embeddedPdfDocument = {new EmbeddedPdfDocument("")};
+    final Notification notification[] = initNotification("");
+    final Random random = new Random();
 
     public IndexView(InvoiceRepository invoiceRepository) {
         this.invoiceRepository = invoiceRepository;
         this.grid = new Grid<>();
-        final EmbeddedPdfDocument[] embeddedPdfDocument = {new EmbeddedPdfDocument("")};
         grid.setItems(invoiceRepository.findAll());
         grid.addColumn(Invoice::getNumber).setHeader("Numer");
         grid.addColumn(Invoice::getDate).setHeader("Data wystawienia");
         grid.addColumn(Invoice::getGrossValue).setHeader("Wartość brutto");
         grid.addComponentColumn(item -> new Button("Podgląd", click -> {
             remove(embeddedPdfDocument[0]);
-            embeddedPdfDocument[0] = new EmbeddedPdfDocument(transformPdfName(item.getNumber()));
-            add(embeddedPdfDocument[0]);
-            if (getHeight() == null || !getHeight().equals("100%")) setHeight("100%");
+            if (random.nextInt(2) == 0) {
+                embeddedPdfDocument[0] = new EmbeddedPdfDocument(transformPdfName(item.getNumber()));
+                add(embeddedPdfDocument[0]);
+                if (getHeight() == null || !getHeight().equals("100%")) setHeight("100%");
+            } else {
+                notification[0] = initNotification(item.getNumber())[0];
+                notification[0].open();
+                setHeight(null);
+            }
         }));
         add(grid);
 
     }
 
-    private String transformPdfName(String invoiceName) {
-        return "pdf/FV_" + invoiceName.substring(3, 4) + "_" + invoiceName.substring(5, 7) + "_" + invoiceName.substring(8) + ".pdf";
+    private String transformPdfName(String invoiceNumber) {
+        return "pdf/FV_" + invoiceNumber.substring(3, 4) + "_" + invoiceNumber.substring(5, 7) + "_" + invoiceNumber.substring(8) + ".pdf";
+    }
+
+    private Notification[] initNotification(String invoiceNumber) {
+        Div content = new Div();
+        content.addClassName("my-style");
+        content.setText("Nie masz wymaganych uprawnień do wyświetlenia treści faktury " + invoiceNumber);
+
+        Notification notification = new Notification(content);
+        notification.setDuration(3000);
+        notification.setPosition(Notification.Position.MIDDLE);
+
+        String styles = ".my-style { "
+                + "  color: red;"
+                + " }";
+
+        StreamRegistration resource = UI.getCurrent().getSession()
+                .getResourceRegistry()
+                .registerResource(new StreamResource("styles.css", () -> {
+                    byte[] bytes = styles.getBytes(StandardCharsets.UTF_8);
+                    return new ByteArrayInputStream(bytes);
+                }));
+        UI.getCurrent().getPage().addStyleSheet(
+                "base://" + resource.getResourceUri().toString());
+
+        return new Notification[]{notification};
     }
 }
